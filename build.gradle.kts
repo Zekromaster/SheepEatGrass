a5f3df85ad75cc608f3df9d791da98e3cbbddc22
Zekromaster <dev@zekromaster.net>
2023-11-19 15:30:44 +0100

    build: Refactor the build.gradle and add SheepEatGrass API dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,9 @@ val loader_version: String by project
 val archives_base_name: String by project
 val next_version: String by project
 val artifact_id: String by project
-
+val use_github_packages = (project.findProperty("gpr.use") as String? ?: "false").toBoolean();
+val gh_username = project.findProperty("gpr.username") as String? ?: System.getenv("GITHUB_ACTOR");
+val gh_token = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN");
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -45,6 +47,20 @@ repositories {
 		name = "Glass Releases"
 		url = uri("https://maven.glass-launcher.net/releases")
 	}
+
+	if (use_github_packages) {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/Zekromaster/*") // Github Package
+			credentials {
+				username = gh_username
+				password = gh_token
+			}
+		}
+	}
+
+	maven(uri("https://jitpack.io"))
+
 	mavenCentral()
 }
 
@@ -55,6 +71,12 @@ dependencies {
 
 	implementation("org.slf4j:slf4j-api:1.8.0-beta4")
 	implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.17.2")
+
+	if (use_github_packages) {
+		modImplementation(include("net.zekromaster.minecraft.sheepeatgrass:sheepeatgrass-api:0.1.0") as Any)
+	} else {
+		modImplementation(include("com.github.Zekromaster:SheepEatGrass.API:0.1.0") as Any)
+	}
 }
 
 tasks {
@@ -75,20 +97,22 @@ tasks.jar {
 	}
 }
 
-publishing {
-	publications {
-		register("mavenJava", MavenPublication::class) {
-			artifactId = artifact_id
-			from(components["java"])
+if (use_github_packages) {
+	publishing {
+		publications {
+			register("mavenJava", MavenPublication::class) {
+				artifactId = artifact_id
+				from(components["java"])
+			}
 		}
-	}
-	repositories {
-		maven {
-			name = "GitHubPackages"
-			url = uri("https://maven.pkg.github.com/Zekromaster/SheepEatGrass") // Github Package
-			credentials {
-				username = System.getenv("GITHUB_ACTOR")
-				password = System.getenv("GITHUB_TOKEN")
+		repositories {
+			maven {
+				name = "GitHubPackages"
+				url = uri("https://maven.pkg.github.com/Zekromaster/SheepEatGrass") // Github Package
+				credentials {
+					username = gh_username
+					password = gh_token
+				}
 			}
 		}
 	}
